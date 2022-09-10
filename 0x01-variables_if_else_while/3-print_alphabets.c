2784390364b56cdeb4997470d53d86eefb09ac56
ibkodus116 <iadewaleodusanya@gmail.com>
2022-09-10 11:38:20 +0000

    fixed

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
-
+/**
+ * main - Print capital A-Z and small a-z on a line
+ * and break next line
+ *
+ * Return: always return 0
+ */
 int main(void)
 {
 	char alpha, cap;
-	for(alpha = 'a'; alpha <= 'z'; alpha++)
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		putchar(alpha);
 	}
-	for(cap = 'A'; cap <= 'Z'; cap++)
+	for (cap = 'A'; cap <= 'Z'; cap++)
 	{
 		putchar(cap);
 	}
-	
 	putchar('\n');
 	return (0);
 }

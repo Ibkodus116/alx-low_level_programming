778df59a707cb707a7a8bd3b006ce9e899a90a64
ibkodus116 <iadewaleodusanya@gmail.com>
2022-09-11 12:51:43 +0000

    final

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,27 +1,47 @@
 #include <stdio.h>
-/*
+
+/**
+ * main - join numbers from 00 to 99.
  *
- *
- *
+ * Return: Always 0 
  */
 int main(void)
 {
-	int a, b;
+	int i, e, g, h, op1, op2;
 
-	for (a = 48; a < 58; a++)
+	i = e = g = h = 48;
+	while (h < 58)
 	{
-		for (b = 48; b < 58; b++)
+		g = 48;
+		while (g < 58)
 		{
-				putchar(a);
-				putchar(a);
-				putchar(' ');
-				if (a != b)
+			e = 48;
+			while (e < 58)
+			{
+				i = 48;
+				while (i < 58)
 				{
-					putchar(a);
-					putchar(b);
-					putchar(',');
+					op1 = (h * 10) + g;
+					op2 = (e * 10) + i;
+					if (op1 < op2)
+					{
+						putchar(h);
+						putchar(g);
+						putchar(' ');
+						putchar(e);
+						putchar(i);
+						if (h == 57 && g == 56 && e == 57 && i == 57)
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+					i++;
 				}
+				e++;
+			}
+			g++;
 		}
+		h++;
 	}
 	putchar('\n');
 	return (0);

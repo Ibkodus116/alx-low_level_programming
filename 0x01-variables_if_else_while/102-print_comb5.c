463924b036c7724906da8d4d76b47afa488342f4
ibkodus116 <iadewaleodusanya@gmail.com>
2022-09-11 13:02:21 +0000

    fixed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,7 @@
 /**
  * main - join numbers from 00 to 99.
  *
- * Return: Always 0 
+ * Return: Always 0
  */
 int main(void)
 {
@@ -25,15 +25,15 @@ int main(void)
 					op2 = (e * 10) + i;
 					if (op1 < op2)
 					{
-						putchar(h);
-						putchar(g);
-						putchar(' ');
-						putchar(e);
-						putchar(i);
-						if (h == 57 && g == 56 && e == 57 && i == 57)
-							break;
-						putchar(',');
-						putchar(' ');
+					putchar(h);
+					putchar(g);
+					putchar(' ');
+					putchar(e);
+					putchar(i);
+					if (h == 57 && g == 56 && e == 57 && i == 57)
+						break;
+					putchar(',');
+					putchar(' ');
 					}
 					i++;
 				}

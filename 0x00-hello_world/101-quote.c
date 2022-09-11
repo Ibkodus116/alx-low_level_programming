3e777f6de5733a9102ab6ecc65406c9ce1d38bdd
ibkodus116 <iadewaleodusanya@gmail.com>
2022-09-11 12:36:59 +0000

    extra

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Print a set of code (start point)
+ *
+ * Return: 1 is returned
+ */
+int main(void)
+{
+	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}

71132a60c9074ea7375ed080e9c94091ee671c1a
ibukun <ibukun@IBK-PC.localdomain>
2022-09-13 15:20:32 +0100

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "1-main.c"
+/**
+ * main - print out the word _putchar
+ *
+ * Return: 0 on success
+ */
+
+
+int main(void)
+{
+    int i = 0;
+
+    for ( i = 97; i < 123; i++){
+        _putchar(i);
+    }
+        _putchar('\n');
+        return(0);
+}

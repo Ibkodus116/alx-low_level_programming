249e460401cc1edbd1b99ff4078763b5776ddb90
ibukun <ibukun@IBK-PC.localdomain>
2022-09-14 18:49:16 +0100

    Betty fixed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,24 @@
 #include "main.h"
-
+/**
+ * print_last_digit - Print the last number
+ * of a given number
+ *
+ * @x: the number we want to check it last value
+ *
+ * Return: the last number
+ */
 int print_last_digit(int x)
 {
-    int f = x % 10;
+	int f = x % 10;
 
-    if (f < 0){
-        _putchar(-f + '0');
-        return (-f);
+	if (f < 0)
+	{
+		_putchar(-f + '0');
+		return (-f);
 
-    }else{
-        return(_putchar(f + '0'));
-    }
+	}
+	else
+	{
+		return (_putchar(f + '0'));
+	}
 }

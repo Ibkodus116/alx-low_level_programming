978fa39b557603172320c26ad21d627e9472bd6e
ibukun <ibukun@IBK-PC.localdomain>
2022-09-16 02:58:12 +0100

    fixed

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,16 @@
 #include "main.h"
 
-int main (void)
+/**
+ * main - tests function that prints
+ * if integer is positive or negative
+ *
+ * Return: Always o
+ */
+int main(void)
 {
-    int i = 0;
-    positive_or_negative(1);
-    return(0);
+	int i = 0;
+
+	positive_or_negative(1);
+
+	return (0);
 }

936dcd41eabc6034a72825ec02b1784d2fefc299
ibukun <ibukun@IBK-PC.localdomain>
2022-10-04 13:34:07 +0100

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated
+ * space in memory
+ *
+ * @str: The given string
+ *
+ * Return: chr on success
+ */
+
+char *_strdup(char *str)
+{
+	char *chr;
+	unsigned int i, j, num = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		num = num + i;
+	}
+
+	chr = (char *)malloc(sizeof(char) * (num));
+
+	if (chr == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+		chr[j] = str[j];
+
+	return (chr);
+}

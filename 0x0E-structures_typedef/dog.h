5364d772d91af9b2aa68227ecf4f58387b371b95
ibukun <ibukun@IBK-PC>
2022-10-11 04:58:08 +0100

    poppy fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,19 @@
-struct dog{
-    char *name;
-    float age;
-    char *owner;
+#ifndef BOY_H
+#define BOY_H
+
+/**
+ * struct dog - Dog attribute
+ * @name: First member
+ * @age: Second member
+ * @owner: Third number
+ * Description: a struct containing a dog attribute.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
+
+#endif

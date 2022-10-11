6789e61f1fd857ac0133e85dcea8428e627f5cb0
ibukun <ibukun@IBK-PC>
2022-10-11 08:14:39 +0100

    fixed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,40 +1,48 @@
 #include "dog.h"
 #include <stdlib.h>
-
+/**
+ * new_dog - create a new dog.
+ * @name:name of dog
+ * @age:age of dog
+ * @owner:owner of dog
+ *
+ * Return: the newly copied dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *my_dog;
-    int i, j, k, x;
-    my_dog = malloc(sizeof(*my_dog));
-    if (!(my_dog) || !(name) || !(owner))
-    {
-        free(my_dog);
-        return (NULL);
-    }
-    for (i = 0; name[i]; i++)
-        ;
-    for (j = 0; owner[j]; j++)
-        ;
+	dog_t *my_dog;
+	int i, j, k, x;
 
-    (*my_dog).name = malloc(i + 1);
-    (*my_dog).owner = malloc(j + 1);
+	my_dog = malloc(sizeof(*my_dog));
+	if (!(my_dog) || !(name) || !(owner))
+	{
+		free(my_dog);
+		return (NULL);
+	}
+	for (i = 0; name[i]; i++)
+		;
+	for (j = 0; owner[j]; j++)
+		;
 
-    if (!(my_dog->name) || !(my_dog->owner))
-    {
-        free(my_dog->owner);
-        free(my_dog->name);
-        free(my_dog);
-        return (NULL);
-    }
-    for (k = 0; k < i; k++)
-        my_dog->name[k] = name[k];
-    my_dog->name[k] = '\0';
+	(*my_dog).name = malloc(i + 1);
+	(*my_dog).owner = malloc(j + 1);
 
-    my_dog->age = age;
+	if (!(my_dog->name) || !(my_dog->owner))
+	{
+		free(my_dog->owner);
+		free(my_dog->name);
+		free(my_dog);
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+		my_dog->name[k] = name[k];
+	my_dog->name[k] = '\0';
 
-    for (x = 0; x < i; x++)
-        my_dog->owner[x] = owner[x];
-    my_dog->owner[x] = '\0';
+	my_dog->age = age;
 
-    return (my_dog);
+	for (x = 0; x < j; x++)
+		my_dog->owner[x] = owner[x];
+	my_dog->owner[x] = '\0';
+
+	return (my_dog);
 }

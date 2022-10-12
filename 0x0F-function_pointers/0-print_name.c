a9884aedb4033af08ec803fab8ddb76493f6a2ec
ibukun <ibukun@IBK-PC>
2022-10-12 17:37:29 +0100

    fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
  * print_name - prints a name.
  * @name: name to be printed
@@ -7,5 +8,5 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-		(*f)(name);
+		f(name);
 }

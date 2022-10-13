a96ae89bc5631569d231f56a9a6b2a13f4cfd6b2
ibukun <ibukun@IBK-PC>
2022-10-13 19:03:07 +0100

    hopefully fixed

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,41 +9,46 @@
 void print_all(const char * const format, ...)
 {
 	char *s;
+	char arr[] = "cifs";
 	int str_len = strlen(format);
 	va_list str;
-	int i, x = 0;
+	int x, j, i = 0;
 
 	va_start(str, format);
 
 	while (i < str_len)
 	{
-		x = 1;
+
+		j = 0;
+		while (arr[j])
+		{
+			if (x && arr[j] == format[i])
+			{
+				printf(", ");
+			}j++;
+		}
 		switch (format[i])
 		{
 		case 'c':
+			x = 1;
 			printf("%c", va_arg(str, int));
 			break;
 		case 'i':
+			x = 1;
 			printf("%d", va_arg(str, int));
 			break;
 		case 'f':
+			x = 1;
 			printf("%f", va_arg(str, double));
 			break;
 		case 's':
+			x = 1;
 			s = va_arg(str, char *);
 			if (!s)
 				s = "(nil)";
 			printf("%s", s);
 			break;
-		default:
-			x = 0;
-			break;
 		}
-		if (x == 1 && i < str_len - 1)
-		{
-			printf(", ");
-		}
-
 		i++;
 	}
 	va_end(str);

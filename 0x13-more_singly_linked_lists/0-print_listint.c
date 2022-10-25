f9b87b8cb800a75e34afd0b811abbd2ab1289cda
ibukun <iadewaleodusanya@gmail.com>
2022-10-25 05:58:42 +0100

    quick fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h)
+	while (h != NULL)
 	{
-		while (h != NULL)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			count++;
-		}
-
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
 	}
+
 	return (count);
 }

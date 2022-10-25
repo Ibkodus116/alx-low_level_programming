02b51a32ecf72e1695e5b60531a344b399780fd4
ibukun <iadewaleodusanya@gmail.com>
2022-10-25 16:41:50 +0100

    [fix] Betty code style

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,36 +1,44 @@
 #include "lists.h"
-
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ *
+ * @head: start of the node
+ * @idx:  index of the list where the new node should be added.
+ * @n: the data in the node.
+ *
+ * Return: ibk (Address of new node)
+*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *start, *ibk;
-    unsigned int i;
+	listint_t *start, *ibk;
+	unsigned int i;
 
-    start = *head;
+	start = *head;
 
-    if (idx != 0)
-    {
-        for (i = 0; i < idx - 1 && start != NULL; i++)
-        {
-            start = start->next;
-        }
-    }
-    if (start == NULL && idx != 0)
-        return (NULL);
+	if (idx != 0)
+	{
+		for (i = 0; i < idx - 1 && start != NULL; i++)
+		{
+			start = start->next;
+		}
+	}
+	if (start == NULL && idx != 0)
+		return (NULL);
 
-    ibk = malloc(sizeof(listint_t));
-    if (ibk == NULL)
-        return (NULL);
+	ibk = malloc(sizeof(listint_t));
+	if (ibk == NULL)
+		return (NULL);
 
-    ibk->n = n;
+	ibk->n = n;
 
-    if (idx == 0)
-    {
-        ibk->next = *head;
-        *head = ibk;
-    } else
-    {
-        ibk->next = start->next;
-        start->next = ibk;
-    }
-    return (ibk);
+	if (idx == 0)
+	{
+		ibk->next = *head;
+		*head = ibk;
+	} else
+	{
+		ibk->next = start->next;
+		start->next = ibk;
+	}
+	return (ibk);
 }

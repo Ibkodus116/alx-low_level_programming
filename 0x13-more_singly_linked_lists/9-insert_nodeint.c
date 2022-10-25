6ed7960a7f7cab8f23a9602827518f219e259cad
ibukun <iadewaleodusanya@gmail.com>
2022-10-25 16:35:33 +0100

    [done] task 9 completed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,19 +2,35 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t ptr, tmp;
+    listint_t *start, *ibk;
+    unsigned int i;
 
-	listint_t *ibk;
+    start = *head;
 
-	ibk = malloc(sizeof(listint_t));
+    if (idx != 0)
+    {
+        for (i = 0; i < idx - 1 && start != NULL; i++)
+        {
+            start = start->next;
+        }
+    }
+    if (start == NULL && idx != 0)
+        return (NULL);
 
-	if (ibk == NULL)
-		return (NULL);
+    ibk = malloc(sizeof(listint_t));
+    if (ibk == NULL)
+        return (NULL);
 
-	ibk->n = n;
-	ibk->next = *head;
+    ibk->n = n;
 
-	*head = ibk;
-	return (*head);
-
+    if (idx == 0)
+    {
+        ibk->next = *head;
+        *head = ibk;
+    } else
+    {
+        ibk->next = start->next;
+        start->next = ibk;
+    }
+    return (ibk);
 }

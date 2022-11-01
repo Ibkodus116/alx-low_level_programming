de979c17f8a26b5691ebb389edd55fe77c0e91b8
ibukun <iadewaleodusanya@gmail.com>
2022-11-01 12:44:15 +0100

    [Done] task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_file - creates a file.
+ *
+ * @filename: name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ *
+ * Return: Success (1) else (0)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i;
+
+	if (!filename)
+		return (-1);
+
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+
+
+	write(fd, text_content, i);
+
+	close(fd);
+	return (1);
+}

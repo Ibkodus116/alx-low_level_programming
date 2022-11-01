c270a1ce9c4f1bf337f6a7f75a5405ecb7483634
ibukun <iadewaleodusanya@gmail.com>
2022-11-01 12:59:45 +0100

    [fix] another error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,12 +22,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		write(fd, "", 1);
+		text_content = "";
 	}
 
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
-
 
 
 	write(fd, text_content, i);

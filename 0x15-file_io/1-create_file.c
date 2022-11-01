b5dc182c4d57ca653c8c8b10e0db86398055d65a
ibukun <iadewaleodusanya@gmail.com>
2022-11-01 12:50:46 +0100

    [fix] task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,13 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 
+    if (text_content != NULL)
+    {
+	    write(fd, "", 1);
+    }
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
+
 
 
 	write(fd, text_content, i);

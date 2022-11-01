efbe8f42d5efa2a6e1ec2f9e0b44ceac6ea308c3
ibukun <iadewaleodusanya@gmail.com>
2022-11-01 21:45:39 +0100

    hope it fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,56 +3,56 @@
  * errors - Error handle.
  * @file_from: Original file.
  * @file_to: Destination.
- * @av: Argument value
+ * @argv: Argument value
  */
 
-void errors(int file_from, int file_to, char **av)
+void errors(int file_from, int file_to, char *argv[])
 {
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
 		exit(99);
 	}
 
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
 		exit(98);
 	}
 }
 
 /**
  * main - ccopies the content of a file to another file.
- * @ac: number of arguments.
- * @av: arguments vector.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
  * Return: Always 0.
  */
 
-int main(int ac, char **av)
+int main(int argc, char *argv[])
 {
 	int file_from, file_to, rd_ff, wr_ft, cl_ft, cl_ff;
 	char *buf;
 
 	buf = malloc(sizeof(char) * 1024);
-	if (ac != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = open(av[1], O_RDONLY);
-	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	errors(file_from, file_to, av);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	errors(file_from, file_to, argv);
 
 	rd_ff = 1024;
 	while (rd_ff == 1024)
 	{
 		rd_ff = read(file_from, buf, 1024);
 		if (rd_ff == -1)
-			errors(-1, 0, av);
+			errors(-1, 0, argv);
 
 		wr_ft = write(file_to, buf, rd_ff);
 		if (wr_ft == -1)
-			errors(0, -1, av);
+			errors(0, -1, argv);
 	}
 
 	cl_ff = close(file_from);

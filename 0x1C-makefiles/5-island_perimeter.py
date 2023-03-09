6b2679c54c67ddd433d1480bce1c6c1c23e5acd2
ibukun <iadewaleodusanya@gmail.com>
2023-03-09 12:54:12 +0100

    extra done

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    if not grid or not grid[0]:
+        return 0  # empty grid
+
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # count the number of adjacent water cells (0s)
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1  # top edge
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1  # left edge
+                if i == rows-1 or grid[i+1][j] == 0:
+                    perimeter += 1  # bottom edge
+                if j == cols-1 or grid[i][j+1] == 0:
+                    perimeter += 1  # right edge
+
+    return perimeter
